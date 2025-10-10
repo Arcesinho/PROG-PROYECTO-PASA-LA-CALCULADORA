823f7478950bc1293b901b764aef3dbd9aa00b11
Adrián Balboa Tomé <a25adrianbt@teis.intranet>
2025-10-10 17:52:30 +0200

    Quitado el tercer jugador y comprobacion de que todo funciona

--- PasaLaCalculadora/src/PasaLaCalculadora.java
+++ PasaLaCalculadora/src/PasaLaCalculadora.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class PasaLaCalculadora {
-
-    //Funcion de un menu para iniciar o acabar el juego
-
+    /**
+     *
+     * @return devuelve la opcion 1 o 2, y vuelve a pedir si no es ninguna de esas
+     */
     public static int getUserInput() {
 
         System.out.println("Bienvenido al juego, que quieres hacer?\n1: Empezar a jugar\n2: Terminar juego");
@@ -17,9 +18,10 @@ public class PasaLaCalculadora {
         }return opcion;
 
     }
-
-    //Función para preguntar por el nombre
-
+    /**
+     *
+     * @return devuelve el nombre del jugador 1
+     */
     public static String getUserName1(){
 
         System.out.println("Introduce tu nombre, jugador 1: ");
@@ -27,7 +29,10 @@ public class PasaLaCalculadora {
 
         return sc.nextLine();
     }
-
+    /**
+     *
+     * @return devuelve el nombre del jugador 2
+     */
     public static String getUserName2(){
 
         System.out.println("Introduce tu nombre, jugador 2: ");
@@ -35,7 +40,10 @@ public class PasaLaCalculadora {
 
         return sc.nextLine();
     }
-    // jugador 3
+    /**
+     *
+     * @return devuelve el nombre del jugador 3
+     */
     public static String getUserName3(){
 
         System.out.println("Introduce tu nombre, jugador 3: ");
@@ -43,9 +51,10 @@ public class PasaLaCalculadora {
 
         return sc.nextLine();
     }
-
-    //Funcion que piide el numero maximo y comprueba si es -1 o no esta en rango
-
+    /**
+     * Con el -1 nos da un numero entre 11 y 99 sin incluir
+     * @return funcion que devuelve el numero maximo
+     */
     public static int getMaxNumber() {
         System.out.println("Introduce un numero del 10 al 99 (introduce -1 si quieres un numero aleatorio): ");
         Scanner sc = new Scanner(System.in);
@@ -70,12 +79,10 @@ public class PasaLaCalculadora {
 
         return maxNumber;
     }
-
-
     /**
      *
-     * @param number1
-     * @param number2
+     * @param number1 = primer numero
+     * @param number2 = segundo numero
      * @return devuelve true si el numero 2, el ultimo se encuentra en la misma fila y columna que el 1 y false si no
      */
     public static boolean getCords(int number1, int number2){
@@ -113,6 +120,10 @@ public class PasaLaCalculadora {
         }
     }
 
+    /**
+     *
+     * @return
+     */
     public static int getFirstNumber(){
 
         System.out.println("Introduce un numero del 1 al 9 (incluidos): ");
@@ -174,106 +185,82 @@ public class PasaLaCalculadora {
                     System.out.println("Error: El número no está en la misma fila o columna. Inténtalo de nuevo.");
                     break;
             }
-
         }
     }
 
     public static void main(String[] args) {
 
-        int opcion = -2;
+        int opcion = -1;
 
 
-        while (opcion != 2){
+        while (opcion != 2) {
 
             int lastNumber = 0;
 
             opcion = getUserInput();
 
-            if (opcion == 2){
+            if (opcion == 2) {
                 System.out.println("Hasta otra!!");
                 break;
             }
-
             String name1 = getUserName1();
             String name2 = getUserName2();
-            // jugador 3
-            String name3 = getUserName3();
             int maxNumber = getMaxNumber();
+            System.out.println("El numero maximo es: "+maxNumber);
             int firstNumber = getFirstNumber();
 
 
-           int total = firstNumber;
+            int total = firstNumber;
 
-           int turno = 1;
+            int turno = 1;
 
-            while (total<= maxNumber){
-
-
-
-                    System.out.println("Turno: "+turno);
-                    System.out.println("Objetivo del juego: "+maxNumber);
-                    System.out.println("Ultimo numero introducido: "+lastNumber);
-                    // 3 jugadores
-                    if (turno % 3 == 1)
-                        System.out.println("Turno del jugador: " + name1);
-                    else if (turno % 3 == 2)
-                        System.out.println("Turno del jugador: " + name2);
-                    else
-                        System.out.println("Turno del jugador: " + name3);
-
-                    System.out.println("Total: "+total);
+            while (total <= maxNumber) {
 
 
-                if (turno == 1){
-                    lastNumber = getNumber(firstNumber);
+                System.out.println("Turno: " + turno);
+                System.out.println("Objetivo del juego: " + maxNumber);
+                System.out.println("Ultimo numero introducido: " + lastNumber);
+                if (turno % 2 == 0) {
+                    System.out.println("Turno del jugador: " + name2);
                 }else {
+                    System.out.println("Turno del jugador: " + name1);
+                }
+                System.out.println("Total: " + total);
+
+                if (turno == 1) {
+                    lastNumber = getNumber(firstNumber);
+                } else {
                     lastNumber = getNumber(lastNumber);
                 }
 
                 turno = turno + 1;
-                if (turno > 1){
-                    total = total + lastNumber ;
+                if (turno > 1) {
+                    total = total + lastNumber;
                 }
-                if (maxNumber == total){
-                    if (turno % 3 == 1) {
-                        System.out.println("El jugador " + name1 + " ha ganado, blackjack!");
-                        System.out.println("llegó a "+ total);
-                        break;
-                    } else if (turno % 3 == 2) {
+
+                if (maxNumber == total) {
+                    if (turno % 2 == 0) {
                         System.out.println("El jugador " + name2 + " ha ganado, blackjack!");
-                        System.out.println("llegó a "+ total);
+                        System.out.println("llegó a " + total);
                         break;
                     } else {
-                        System.out.println("El jugador " + name3 + " ha ganado, blackjack!");
-                        System.out.println("llegó a "+ total);
+                        System.out.println("El jugador " + name1 + " ha ganado, blackjack!");
+                        System.out.println("llegó a " + total);
                         break;
                     }
-
                 }
-                if (maxNumber< total) {
-                    if (turno % 3 == 0) {
-                        System.out.println("El jugador " + name3 + " ha ganado la partida!!");
-                        System.out.println("Con " + total);
-                        break;
-                    } else if (turno % 3 == 2) {
+                if (total > maxNumber) {
+                    if (turno % 2 == 0) {
                         System.out.println("El jugador " + name1 + " ha ganado la partida!!");
-                        System.out.println("Con" + total);
+                        System.out.println("El jugador " + name2 + " se ha pasado\n");
                         break;
-
                     } else {
                         System.out.println("El jugador " + name2 + " ha ganado la partida!!");
-                        System.out.println("Con" + total);
+                        System.out.println("El jugador " + name1 + " se ha pasado\n");
                         break;
                     }
-
-
                 }
-
-
             }
-
-
         }
     }
 }
-// no para al llegar al numero maximo
